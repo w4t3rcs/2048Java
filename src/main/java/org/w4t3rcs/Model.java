b17db6795f12c07a78f930c6a75865860d0aad87
w4t3rcs <w4t3rofficial@gmail.com>
2023-06-08 21:25:38 +0200

    Added tile array (game field) to Model class!

--- src/main/java/org/w4t3rcs/Model.java
+++ src/main/java/org/w4t3rcs/Model.java
@@ -1,4 +1,13 @@
 package org.w4t3rcs;
 
+import java.util.Arrays;
+
 public class Model {
+    private static final int FIELD_WIDTH = 4;
+    private Tile[][] gameTiles;
+
+    public Model() {
+        this.gameTiles = new Tile[FIELD_WIDTH][FIELD_WIDTH];
+        Arrays.stream(gameTiles).forEach(gameTile -> Arrays.setAll(gameTile, j -> new Tile()));
+    }
 }
